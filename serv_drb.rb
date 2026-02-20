896584f9fbe82e257671976c1a2ca4f6a2d4f129
IgorSechkin <40743083+IgorSechkin@users.noreply.github.com>
2026-02-20 19:27:31 +0200

    Restore required libraries and service initialization

--- serv_drb.rb
+++ serv_drb.rb
@@ -117,7 +117,7 @@ class MyService
         # кукки email и password для profile 
         # str = "<link rel=\"stylesheet\" href=\"email=#{@us[2]}\"> <link rel=\"stylesheet\" href=\"password=#{@us[6]}\"> <meta http-equiv=\"Refresh\" content=\"0; URL=show_tovar\"/>"
         # return str
-        return ERB.new(IO.read("./delete_session_cookie.erb")).result(binding)
+        return ERB.new(IO.read("./public/delete_session_cookie.html.erb")).result(binding)
       else
         return "<p style=\"color: red;\">Ви не зареєстровані, або не вірний пароль</p>"
       end
@@ -319,6 +319,7 @@ puts "Сервер запущен на druby://:9000"
 DRb.thread.join
 
 # system("kill #{Process.pid}")
+
 
 
 
