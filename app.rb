e8ef7cc17702f0a795d6483c2d0cfcaf748d2d0f
IgorSechkin <40743083+IgorSechkin@users.noreply.github.com>
2026-02-20 22:00:38 +0200

    Refactor App class and restore DRb server functionality

--- app.rb
+++ app.rb
@@ -43,6 +43,7 @@ class App
       # p meth
       # "<meta http-equiv=\"Refresh\" content=\"0; URL=home.html\"/>"
       coockie = "HTTP/1.1 200 OK\r\nContent-Type: text/css\r\nSet-Cookie: /#{meth}; Path=/; secure=true; SameSite=none; HttpOnly=true;"
+      p coockie
       sock.write coockie 
       sock.close
     else
@@ -94,4 +95,5 @@ end
 # env["HTTP_COOKIE"]
 # "CONTENT_LENGTH"=>"25"
 # env["rack.input"]
+
 
