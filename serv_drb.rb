44a353060dfec19a3525e96967f11764f3c09101
IgorSechkin <40743083+IgorSechkin@users.noreply.github.com>
2026-02-21 09:48:30 +0200

    Refactor MyService class and restore requires

--- serv_drb.rb
+++ serv_drb.rb
@@ -119,7 +119,7 @@ class MyService
         # return str
         @email = "/email=#{@us[2]}"
         @password = "/password=#{@us[6]}"
-        return ERB.new(IO.read("./public/set_session_cookie.html.erb")).result(binding)
+        return ERB.new(IO.read("./public/set_cookie_session.html.erb")).result(binding)
       else
         return "<p style=\"color: red;\">Ви не зареєстровані, або не вірний пароль</p>"
       end
@@ -321,6 +321,7 @@ puts "Сервер запущен на druby://:9000"
 DRb.thread.join
 
 # system("kill #{Process.pid}")
+
 
 
 
